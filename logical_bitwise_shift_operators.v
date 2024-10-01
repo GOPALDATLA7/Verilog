df8e8c6e98f259080f7d9d7efca7fae2e93e76e4
GOPALDATLA7 <115442333+GOPALDATLA7@users.noreply.github.com>
2024-10-01 11:55:57 +0530

    Update logical_bitwise_shift_operators.v

--- logical_bitwise_shift_operators.v
+++ logical_bitwise_shift_operators.v
@@ -1,3 +1,5 @@
+/LOGICAL OPEARTORS/
+AND - && , OR - || , NOT - !
 module logical();
     reg a,b,c;
     reg x,y,z;
@@ -10,3 +12,9 @@ module logical();
             $display("x = %b, y = %b, z = %b",x,y,z);
         end
 endmodule
+
+////
+Output    x=1 , y=x, z=0
+1-TRUE,  0-FALSE, X-UNKNOWN
+Explaination: x=1 because both the inputs are high, y=x because one of the input is unknown, z=0 because one of the input is low so it fails.
+////
