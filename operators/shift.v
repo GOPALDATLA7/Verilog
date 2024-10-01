d696aba2e194e45004b3aff6ca6259281530d526
GOPALDATLA7 <115442333+GOPALDATLA7@users.noreply.github.com>
2024-10-01 15:18:38 +0530

    Update shift.v

--- operators/shift.v
+++ operators/shift.v
@@ -4,10 +4,13 @@ module shift();
     reg [3:0] a=4'b1001,b=4'b0111,w,x,y,z;
     initial
         begin
-            w = a << 1
+            w = a << 1;
             x = b <<< 1;
             y = a >> 2;
             z = b >>> 2;
             $display("w = %b,x = %b, y = %b, z = %b",w,x,y,z);
         end
 endmodule
+/*
+Output: w= 0010, x= 1110, y= 0010, z= 0001
+*/
