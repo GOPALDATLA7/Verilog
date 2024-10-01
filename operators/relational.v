782c523c132afe8a7d06cf01dedc2ac9baed273f
GOPALDATLA7 <115442333+GOPALDATLA7@users.noreply.github.com>
2024-10-01 09:28:25 +0000

    relational operators

---
+++ operators/relational.v
@@ -0,0 +1,13 @@
+/*RELATIONAL OPERATORS*/
+/*< <= >= >*/
+module shift();
+    reg a=4,b=10,w,x,y,z;
+    initial
+        begin
+            w = a < b
+            x = b <= a;
+            y = a > b;
+            z = b >= a;
+            $display("w = %b,x = %b, y = %b, z = %b",w,x,y,z);
+        end
+endmodule
