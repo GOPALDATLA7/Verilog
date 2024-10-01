a68950fcdefcf537130ff4a138dad05d13da1526
GOPALDATLA7 <115442333+GOPALDATLA7@users.noreply.github.com>
2024-10-01 15:22:59 +0530

    Update arthmetic.v

--- operators/arthmetic.v
+++ operators/arthmetic.v
@@ -1,12 +1,14 @@
-
-module relational();
-    reg a=4,b=10,w,x,y,z;
+/*ARTHMETIC OPERATIONS*/
+module arthmetic();
+    reg [3:0]a=4,b=10,w,x,y,z;
     initial
         begin
-            w = a + b
+            w = a + b;
             x = b - a;
             y = a * b;
             z = b % a;
             $display("w = %b,x = %b, y = %b, z = %b",w,x,y,z);
         end
 endmodule
+/*
+Output: w = 0111,x = 1111, y = 1100, z = 0011*/
