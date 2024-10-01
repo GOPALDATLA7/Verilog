e28ef61627360ebcacd22592aa32527fdbea4361
GOPALDATLA7 <115442333+GOPALDATLA7@users.noreply.github.com>
2024-10-01 15:13:27 +0530

    Update relational.v

--- operators/relational.v
+++ operators/relational.v
@@ -1,13 +1,17 @@
 /*RELATIONAL OPERATORS*/
 /*< <= >= >*/
 module relational();
-    reg a=4,b=10,w,x,y,z;
+    reg [3:0]a=4,b=10;
+    reg w,x,y,z;
     initial
         begin
-            w = a < b
+            w = a < b;
             x = b <= a;
             y = a > b;
             z = b >= a;
             $display("w = %b,x = %b, y = %b, z = %b",w,x,y,z);
         end
 endmodule
+
+/* Output:  w=1, x=0, y=0, z=1
+Explanation: If the relation between both the operands is true then the output is HIGH else LOW*/
