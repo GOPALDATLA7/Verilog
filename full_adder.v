48cab1a58981f19ebdb4357d7795cb2916d6fff1
GOPALDATLA7 <115442333+GOPALDATLA7@users.noreply.github.com>
2024-10-02 11:47:18 +0000

    Full adder rtl and tb

---
+++ full_adder.v
@@ -0,0 +1,28 @@
+//Full Adder
+//RTL
+module full_adder(
+    input a,b,cin,
+    output sum,carry);
+    
+    assign sum = a^b^cin;  //Xor Operation
+    assign carry = (a&b)|(cin&b)|(a&cin);
+endmodule
+
+//TB
+module full_adder_tb();
+reg a,b,cin;
+wire sum,carry;
+full_adder dut(a,b,cin,sum,carry);
+initial
+    begin
+        a=0;b=0;cin=0;
+        #10 a=0;b=0;cin=1;
+        #10 a=0;b=1;cin=0;
+        #10 a=0;b=1;cin=1;
+        #10 a=1;b=0;cin=0;
+        #10 a=1;b=0;cin=1;
+        #10 a=1;b=1;cin=0;
+        #10 a=1;b=1;cin=1;
+        #10 $finish;
+    end
+endmodule
